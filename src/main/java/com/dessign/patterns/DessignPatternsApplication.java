10b494fac60836c71c0583d1021611cbf248fe6f
moslisnas <molicapacidad@gmail.com>
2023-11-26 17:52:24 +0100

    Initial commit

---
+++ src/main/java/com/dessign/patterns/DessignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.dessign.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DessignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dessign/patterns/DessignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dessign.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DessignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
